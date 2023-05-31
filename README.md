5bccf648de843e7a02fdeea2f7158845e6366776
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-05-31 19:49:38 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ToDo List
+
+This is assignment 2 of the LNU course 1DV535.
---
+++ android/app/src/main/kotlin/tech/neurothrone/todo_list/MainActivity.kt
@@ -0,0 +1,6 @@
+package tech.neurothrone.todo_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
